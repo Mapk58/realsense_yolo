701f801453c2aa9a9d40ba9b612ffd922825029b
Mark Vishnevsky <markpenza@gmail.com>
2024-10-05 14:27:52 +0300

    some refactoring

--- driver.py
+++ driver.py
@@ -20,94 +20,18 @@ class RealSenseCamera:
         align_to = rs.stream.color
         self.align = rs.align(align_to)
 
-    def get_intrinsics(self):
-        frames = self.pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        depth_intrinsics = depth_frame.profile.as_video_stream_profile().intrinsics
-        return depth_intrinsics
-
-    def get_color_image(self):
-        for _ in range(5):
-            frames = self.pipeline.wait_for_frames()
-        frames = self.pipeline.wait_for_frames()
-        aligned_frames = self.align.process(frames)
-        color_frame = aligned_frames.get_color_frame()
-        # color_frame = frames.get_color_frame()
-        if not color_frame:
-            return None
-        color_image = np.asanyarray(color_frame.get_data())
-        return color_image
-
-    def get_depth_map(self, distance=True):
-        for _ in range(3):
-            frames = self.pipeline.wait_for_frames()
-        frames = self.pipeline.wait_for_frames()
-        aligned_frames = self.align.process(frames)
-        depth_frame = aligned_frames.get_depth_frame()
-        # depth_frame = frames.get_depth_frame()
-        if not depth_frame:
-            return None
-        depth_map = np.asanyarray(depth_frame.get_data())
-        frame_shape = depth_map.shape
-        if distance:
-            distance_array = np.zeros(frame_shape)
-            for i in range(frame_shape[0]):
-                for j in range(frame_shape[1]):
-                    distance_array[i][j] = depth_frame.get_distance(j, i)  
-            distance_map = np.asanyarray(distance_array)
-            return depth_map, distance_map
-        return depth_map
+    # def get_intrinsics(self):
+    #     frames = self.pipeline.wait_for_frames()
+    #     depth_frame = frames.get_depth_frame()
+    #     depth_intrinsics = depth_frame.profile.as_video_stream_profile().intrinsics
+    #     return depth_intrinsics
 
     def get_frame_from_file(self, path=""):
         data = {}
         data['color_image'] = np.load(path + '/color_image.npy')
         data['depth_accuracy'] = np.load(path + '/depth_accuracy.npy')
-        # data['distance_accuracy'] = np.load(path + '/distance_accuracy.npy')
         data['depth_density'] = np.load(path + '/depth_density.npy')
         return data
-
-    def _get_frame_data(self, depth_row=20):
-        data = {}
-        data['color_image'] = self.get_color_image()
-
-        self.depth_sensor.set_option(rs.option.visual_preset, 2)
-        depths = []
-        distances = []
-        for frame_number in range(depth_row):
-            depth_map, distance_map = self.get_depth_map(True)
-            depth_unique, depth_counts = np.unique(depth_map.flatten(), return_counts=True)
-            distance_unique, distance_counts = np.unique(distance_map.flatten(), return_counts=True)
-            if depth_map is not None:
-                depths.append(depth_map)
-                distances.append(distance_map)
-        
-        if len(depths) > 0:
-            stacked_depths = np.stack(depths)
-            mean_array = np.where(np.count_nonzero(stacked_depths, axis=0) > 0, np.sum(stacked_depths, axis=0) / np.count_nonzero(stacked_depths, axis=0),0)
-            data['depth_accuracy'] = mean_array
-            stacked_distances = np.stack(distances)
-            mean_array = np.where(np.count_nonzero(stacked_distances, axis=0) > 0, np.sum(stacked_distances, axis=0) / np.count_nonzero(stacked_distances, axis=0),0)
-            data['distance_accuracy'] = mean_array
-
-        self.depth_sensor.set_option(rs.option.visual_preset, 3)
-        depths = []
-        distances = []
-        for frame_number in range(depth_row):
-            depth_map, distance_map = self.get_depth_map(True)
-            if depth_map is not None:
-                depths.append(depth_map)
-                distances.append(distance_map)
-        
-        if len(depths) > 0:
-            stacked_depths = np.stack(depths)
-            mean_array = np.where(np.count_nonzero(stacked_depths, axis=0) > 0, np.sum(stacked_depths, axis=0) / np.count_nonzero(stacked_depths, axis=0),0)
-            data['depth_density'] = mean_array
-            stacked_distances = np.stack(distances)
-            mean_array = np.where(np.count_nonzero(stacked_distances, axis=0) > 0, np.sum(stacked_distances, axis=0) / np.count_nonzero(stacked_distances, axis=0),0)
-            data['distance_density'] = mean_array
-        
-        return data
-    
     
     def get_frame_data(self):
     
@@ -137,7 +61,6 @@ class RealSenseCamera:
         data = {}
         data['color_image'] = np.array(color_image)
         data['depth_density'] = np.array(depth_image)
-        data['distance_density'] = np.array(depth_image)
 
         self.depth_sensor.set_option(rs.option.visual_preset, 2)        
         full_depth = np.zeros((480, 640))
@@ -160,7 +83,6 @@ class RealSenseCamera:
         color_image = color_temp
 
         data['depth_accuracy'] = np.array(depth_image)
-        data['distance_accuracy'] = np.array(depth_image)
         
         return data
 
@@ -171,25 +93,15 @@ class RealSenseCamera:
             cv2.imwrite(os.path.join(frame_name, 'color_image.png'), data['color_image'])
             np.save(os.path.join(frame_name, 'color_image.npy'), data['color_image'])
 
-        normalized_depth = cv2.normalize(data['depth_accuracy'], None, 0, 255, cv2.NORM_MINMAX)
-        normalized_depth = np.uint8(normalized_depth)
-        cv2.imwrite(os.path.join(frame_name, f'depth_accuracy.png'), normalized_depth)
+        normalized_depth_accuracy = cv2.normalize(data['depth_accuracy'], None, 0, 255, cv2.NORM_MINMAX)
+        normalized_depth_accuracy = np.uint8(normalized_depth_accuracy)
+        cv2.imwrite(os.path.join(frame_name, f'depth_accuracy.png'), normalized_depth_accuracy)
         np.save(os.path.join(frame_name, f'depth_accuracy.npy'), data['depth_accuracy'])
 
-        normalized_depth = cv2.normalize(data['depth_density'], None, 0, 255, cv2.NORM_MINMAX)
-        normalized_depth = np.uint8(normalized_depth)
-        cv2.imwrite(os.path.join(frame_name, f'depth_density.png'), normalized_depth)
+        normalized_depth_density = cv2.normalize(data['depth_density'], None, 0, 255, cv2.NORM_MINMAX)
+        normalized_depth_density = np.uint8(normalized_depth_density)
+        cv2.imwrite(os.path.join(frame_name, f'depth_density.png'), normalized_depth_density)
         np.save(os.path.join(frame_name, f'depth_density.npy'), data['depth_density'])
-
-        normalized_distance = cv2.normalize(data['distance_accuracy'], None, 0, 255, cv2.NORM_MINMAX)
-        normalized_distance = np.uint8(normalized_distance)
-        cv2.imwrite(os.path.join(frame_name, f'distance_accuracy.png'), normalized_distance)
-        np.save(os.path.join(frame_name, f'distance_accuracy.npy'), data['distance_accuracy'])
-
-        normalized_distance = cv2.normalize(data['distance_density'], None, 0, 255, cv2.NORM_MINMAX)
-        normalized_distance = np.uint8(normalized_distance)
-        cv2.imwrite(os.path.join(frame_name, f'distance_density.png'), normalized_distance)
-        np.save(os.path.join(frame_name, f'distance_density.npy'), data['distance_density'])
 
     def stop(self):
         self.pipeline.stop()
